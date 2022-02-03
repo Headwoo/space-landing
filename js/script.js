3563bdaac8996fdf3ceabf2498f67c707a3e5975
YURA <yurafdfd@gmail.com>
2022-02-03 16:32:29 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const iconMenu = document.querySelector(".menu__icon");
 if (iconMenu) {
-    const menuBody = document.querySelector('.header__nav');
+    const menuBody = document.querySelector('.header__menu');
     iconMenu.addEventListener("click", function(e){
         document.body.classList.toggle("_lock");
         iconMenu.classList.toggle("_active");
